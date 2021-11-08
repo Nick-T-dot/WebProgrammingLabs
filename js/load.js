90f7020985be42c28a10e59b4ba3f290e43c4f04
Nick-T-dot <71936623+Nick-T-dot@users.noreply.github.com>
2021-11-08 14:55:04 +0300

    Update load.js

--- js/load.js
+++ js/load.js
@@ -11,14 +11,14 @@ try {
 		newText = document.createTextNode("Loaded in " + load.toString() + " ms");
 		document.getElementsByTagName("FOOTER")[0].appendChild(newText);
 	
-		loc = document.location.toString().split("/");
-		loc = loc[loc.length - 1]
+		let loc = document.location.toString().split("/");
+		let loc = loc[loc.length - 1]
 		var menuItems = document.getElementById("headmenu").getElementsByTagName("A");
 		switch(loc)
 		{
-		case "index.html" : menuItems[0].style.background = "#AAAAAA"; break;
-		case "info.html" : menuItems[1].style.background = "#AAAAAA"; break;
-		case "blank.html" : menuItems[2].style.background = "#AAAAAA"; break;
+		case "index.html" : menuItems[0].classList.add(".actmenu"); break;
+		case "info.html" : menuItems[1].classList.add(".actmenu"); break;
+		case "blank.html" : menuItems[2].classList.add(".actmenu"); break;
 		}
 	};
 }
