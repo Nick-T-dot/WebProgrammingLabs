fed2f2516a4fbb8daeb69845b262cc50080fea78
Nick-T-dot <71936623+Nick-T-dot@users.noreply.github.com>
2021-12-06 14:54:26 +0300

    Add files via upload

--- js/net.js
+++ js/net.js
@@ -1,8 +1,11 @@
 window.addEventListener("load", function(event) {
     var load = Math.floor(Math.random() * 2);
-    var nam = "images/load" + load.toString() + ".gif";
+    var nam = `images/load${load.toString()}.gif`;
     document.getElementById('loader').setAttribute("src",nam);
-    var prom = fetch('https://jsonplaceholder.typicode.com/comments');
+    var id = Math.floor(Math.random() * 100);
+    var lin = `https://jsonplaceholder.typicode.com/comments?postId=${id}`
+    console.log(lin)
+    var prom = fetch(lin);
     prom.then(
         response => response.json(),
         error => reject()
@@ -11,16 +14,15 @@ window.addEventListener("load", function(event) {
   
 function resolve(response)
 {
-    var id = 0;
+    console.log(response)
     var el = null;
     var comm = null;
     document.getElementById('loader').hidden = "hidden";
     let div = document.getElementById("commlist")
-    for (var i = 0; i < 10; i++)
+    let len = response.length
+    for (var i = 0; i < len; i++)
     {
-        id = Math.floor(Math.random() * 200)
-        comm = response[id]
-        console.log(comm);
+        comm = response[i]
         el = document.getElementById('comment').content.cloneNode(true).childNodes[1]
         el.getElementsByTagName('P')[0].textContent += comm.name
         el.getElementsByTagName('P')[1].textContent += comm.email
